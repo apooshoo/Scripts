34a083e89ab0ea0b60625de6f2efe2615aa93110
Jonathan Bek <jonathanbek93@hotmail.com>
2024-03-13 17:02:52 +0800

    Refactored out logging code to partial

---
+++ Scripter/MainWindow.Logger.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Concurrent;
+
+namespace Scripter
+{
+    partial class MainWindow
+    {
+        private int _whileLoopCountTest = 0;
+        private void WriteLogsToUiPeriodically(ConcurrentQueue<string> log, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                _whileLoopCountTest++;
+                TryWriteLogToUI(log);
+                Thread.Sleep(100);
+            }
+        }
+
+        private void TryWriteLogToUI(ConcurrentQueue<string> log)
+        {
+            if (log.TryDequeue(out var message))
+            {
+                if (!string.IsNullOrEmpty(message))
+                {
+                    WriteLogToUi(message);
+                }
+            }
+        }
+
+        private void WriteRemainingLogs(ConcurrentQueue<string> log)
+        {
+            while (!log.IsEmpty)
+            {
+                TryWriteLogToUI(log);
+            }
+        }
+
+        private void WriteLogToUi(string message)
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                Output.AppendText(message);
+                Output.AppendText(Environment.NewLine);
+            }));
+        }
+    }
+}
--- Scripter/MainWindow.xaml.cs
+++ Scripter/MainWindow.xaml.cs
@@ -66,45 +66,6 @@ namespace Scripter
             WriteLogToUi("Operation complete. Test count: " + _whileLoopCountTest);
         }
 
-        private int _whileLoopCountTest = 0;
-        private void WriteLogsToUiPeriodically(ConcurrentQueue<string> log, CancellationToken token)
-        {
-            while (!token.IsCancellationRequested)
-            {
-                _whileLoopCountTest++;
-                TryWriteLogToUI(log);
-                Thread.Sleep(50);
-            }
-        }
-
-        private void TryWriteLogToUI(ConcurrentQueue<string> log)
-        {
-            if (log.TryDequeue(out var message))
-            {
-                if (!string.IsNullOrEmpty(message))
-                {
-                    WriteLogToUi(message);
-                }
-            }
-        }
-
-        private void WriteRemainingLogs(ConcurrentQueue<string> log)
-        {
-            while (!log.IsEmpty)
-            {
-                TryWriteLogToUI(log);
-            }
-        }
-
-        private void WriteLogToUi(string message)
-        {
-            Dispatcher.BeginInvoke(new Action(() =>
-            {
-                Output.AppendText(message);
-                Output.AppendText(Environment.NewLine);
-            }));
-        }
-
         private void Trim(string[] folders, ConcurrentQueue<string> log)
         {
             if (TrimCheckBox.IsChecked.GetValueOrDefault())
