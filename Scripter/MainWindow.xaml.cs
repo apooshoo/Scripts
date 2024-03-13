5c0229127e1e04e207d64095e7b2ba295cd34d9a
Jonathan Bek <jonathanbek93@hotmail.com>
2024-03-13 18:51:14 +0800

    Small UI refactoring and change

--- Scripter/MainWindow.xaml.cs
+++ Scripter/MainWindow.xaml.cs
@@ -104,13 +104,13 @@ namespace Scripter
             }
         }
 
-        private void Trim_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        private void Trim_PreviewTextInput_ParseNumber(object sender, TextCompositionEventArgs e)
         {
             var textBox = sender as TextBox;
             e.Handled = Regex.IsMatch(e.Text, "[^0-9]") && e.Text.Length <= 2; 
         }
 
-        private void Trim_GotFocus(object sender, RoutedEventArgs e)
+        private void TextBox_GotFocus_SelectAll(object sender, RoutedEventArgs e)
         {
             var textBox = sender as TextBox;
             textBox.Dispatcher.BeginInvoke(new Action(() => textBox.SelectAll()));
