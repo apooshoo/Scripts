b3faf43bcbde52830c0c595439b0a019930ed673
Jonathan Bek <jonathanbek93@hotmail.com>
2024-12-29 17:45:12 +0800

    Converted to in-app imagemagick

--- Scripts/FileConverter.cs
+++ Scripts/FileConverter.cs
@@ -1,25 +1,27 @@
-﻿using System.Collections.Concurrent;
-using System.Diagnostics;
+﻿using ImageMagick;
+using Microsoft.VisualBasic.FileIO;
+using System.Collections.Concurrent;
 
 namespace Scripts
 {
     public static class FileConverter
     {
-        const string exePath = @"C:\Users\jonat\Downloads\New folder\ImageMagick-7.1.1-43-portable-Q16-x64\ImageMagick-7.1.1-43-portable-Q16-x64\magick.exe";
-        const string exeCmd = "mogrify -format jpeg *.webp";
-
         public static void ConvertWebps(string folderPath, IProducerConsumerCollection<string> log)
         {
             try
             {
-                string cmd1 = $"&'{exePath}' {exeCmd}";
-                string cmd2 = "del *.webp";
-
                 if (NeedsConverting(folderPath))
                 {
+                    var files = FileSystem.GetFiles(folderPath).Select(f => FileSystem.GetFileInfo(f));
+                    var queuedFiles = new Queue<FileInfo>(files);
+
                     log.TryAdd("Converting: " + folderPath);
-                    ExecuteCommand(cmd1, folderPath, log);
-                    ExecuteCommand(cmd2, folderPath, log);
+                    ConvertFiles(queuedFiles);
+
+                    log.TryAdd("Done converting. Deleting old files...");
+                    DeleteFiles(queuedFiles);
+
+                    log.TryAdd("Done.");
                 }
                 else
                 {
@@ -32,31 +34,27 @@ namespace Scripts
             }
         }
 
-        private static void ExecuteCommand(string command, string folderPath,
-            IProducerConsumerCollection<string> log)
-        {
-            var processStartInfo = new ProcessStartInfo();
-            processStartInfo.FileName = "powershell.exe";
-            processStartInfo.WorkingDirectory = folderPath;
-            processStartInfo.Arguments = $"-Command \"{command}\"";
-            processStartInfo.UseShellExecute = false;
-            processStartInfo.RedirectStandardOutput = true;
-            processStartInfo.CreateNoWindow = true;
-
-            using var process = new Process();
-            process.StartInfo = processStartInfo;
-            process.Start();
-            string output = process.StandardOutput.ReadToEnd();
-            if (!string.IsNullOrEmpty(output))
-            {
-                log.TryAdd("Powershell output: " + output);
-            }
-        }
-
         private static bool NeedsConverting(string path)
         {
             var files = Directory.GetFiles(path);
             return files.Any(f => Path.GetExtension(f).Contains("webp"));
+        }
+
+        private static void ConvertFiles(Queue<FileInfo> queuedFiles)
+        {
+            foreach (var file in queuedFiles)
+            {
+                using var image = new MagickImage(file);
+                image.Write(file.FullName.Replace(".webp", ".jpeg"));
+            }
+        }
+
+        private static void DeleteFiles(Queue<FileInfo> queuedFiles)
+        {
+            while (queuedFiles.TryDequeue(out var file))
+            {
+                file.Delete();
+            }
         }
     }
 }
---
+++ Scripts/Unused/PowershellScripter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace Scripts.Unused
+{
+    internal class PowershellScripter
+    {
+        private static void ExecuteCommand(string command, string fromFolderPath,
+            IProducerConsumerCollection<string> log)
+        {
+            var processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = "powershell.exe";
+            processStartInfo.WorkingDirectory = fromFolderPath;
+            processStartInfo.Arguments = $"-Command \"{command}\"";
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.CreateNoWindow = true;
+
+            using var process = new Process();
+            process.StartInfo = processStartInfo;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            if (!string.IsNullOrEmpty(output))
+            {
+                log.TryAdd("Powershell output: " + output);
+            }
+        }
+    }
+}
